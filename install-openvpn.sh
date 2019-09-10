ebe64563c4358da749a867c5416d47ed95c59c45
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-10 18:58:47 +0530

    Create install-openvpn.sh

---
+++ install-openvpn.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
+# Date        : 10 September 2019,
+# Description : Installing / Compiling OpenVPN from Source Code.
+
+# update package repositories.
+sudo yum -y update
+
+# installing build essentials.
+sudo yum -y install openssl-devel lzo-devel pam-devel
+
+# openvpn downloading, extracting and compiling.
+curl -JL https://swupdate.openvpn.org/community/releases/openvpn-2.4.7.tar.gz -o $HOME/openvpn.tar.gz && \
+tar -xzf $HOME/openvpn.tar.gz -C $HOME                                                                && \
+rm  -rf  $HOME/openvpn.tar.gz                                                                         && \
+mv       $HOME/openvpn-* $HOME/OpenVPN                                                                && \
+cd       $HOME/OpenVPN                                                                                && \
+./configure --prefix=$HOME/openvpn --disable-lz4                                                      && \
+make                                                                                                  && \
+make install                                                                                          && \
+cd       $HOME                                                                                        && \
+rm  -rf  $HOME/OpenVPN                                                                                && \
+echo "export PATH=$PATH:$HOME/openvpn/sbin" >> $HOME/.bashrc                                          && \
+exec $BASH
