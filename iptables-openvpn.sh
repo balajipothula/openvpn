1b9eed2b6ac0933d8d75aed9e8e39875d080e823
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-11 12:50:01 +0530

    Create iptables-openvpn.sh

---
+++ iptables-openvpn.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Author      : BALAJI POTHULA (balaji.pothula@techie.com),
+# Date        : 29 April 2018,
+# Description : Setting openvpn server firewall.
+
+# flushing all rules.
+iptables -F
+
+# accepting out going traffic. 
+iptables -P OUTPUT ACCEPT
+
+# iptables forwarding policy acceptance.
+iptables -P FORWARD ACCEPT
+
+# accept ssh connections from any ip.
+iptables -A INPUT -m state --state NEW -p tcp --dport   22 -j ACCEPT
+
+# accept openvpn connections from any ip.
+iptables -A INPUT -m state --state NEW -p udp --dport 1194 -j ACCEPT
+
+# routing traffic through openvpn server.
+# 10.8.0.0, 10.8.1.0, ..., 10.8.255.0
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+
+# tracking connection.
+# packets relationship to previous connections.
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
